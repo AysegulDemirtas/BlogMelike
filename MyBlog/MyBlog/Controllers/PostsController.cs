10d1d9ec0647534f4e5487933596637e1c5253ff
MelikeKarabulut <karabulut.melike@gmail.com>
2015-06-05 16:22:35 +0300

    Make a simple blog

---
+++ MyBlog/MyBlog/Controllers/PostsController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyBlog.Models;
+
+namespace MyBlog.Controllers
+{
+    public class PostsController : Controller
+    {
+        private PostsDBContext db = new PostsDBContext();
+
+        // GET: Posts
+        public ActionResult Index(string postsGenre, string searchString)
+        {
+            var GenreLst = new List<string>();
+
+            var GenreQry = from d in db.Posts
+                           orderby d.Genre
+                           select d.Genre;
+
+            GenreLst.AddRange(GenreQry.Distinct());
+            ViewBag.movieGenre = new SelectList(GenreLst);
+
+            var movies = from m in db.Posts
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(postsGenre))
+            {
+                movies = movies.Where(x => x.Genre == postsGenre);
+            }
+
+            return View(movies);
+        }
+
+        // GET: Posts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Posts posts = db.Posts.Find(id);
+            if (posts == null)
+            {
+                return HttpNotFound();
+            }
+            return View(posts);
+        }
+
+        // GET: Posts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Posts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Posts posts)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Posts.Add(posts);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(posts);
+        }
+
+        // GET: Posts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Posts posts = db.Posts.Find(id);
+            if (posts == null)
+            {
+                return HttpNotFound();
+            }
+            return View(posts);
+        }
+
+        // POST: Posts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Posts posts)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(posts).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(posts);
+        }
+
+        // GET: Posts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Posts posts = db.Posts.Find(id);
+            if (posts == null)
+            {
+                return HttpNotFound();
+            }
+            return View(posts);
+        }
+
+        // POST: Posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Posts posts = db.Posts.Find(id);
+            db.Posts.Remove(posts);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
